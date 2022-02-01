f9b020f375f247497c6d6b3e7cd417038d0bcb61
Neto Nascimento <netolima1992@gmail.com>
2022-02-01 17:08:14 -0300

    Service layer, component registration

--- src/main/java/com/jneto/cgae/resources/PublicoResource.java
+++ src/main/java/com/jneto/cgae/resources/PublicoResource.java
@@ -1,19 +1,32 @@
 package com.jneto.cgae.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jneto.cgae.entities.Publico;
+import com.jneto.cgae.services.PublicoService;
 
 @RestController
 @RequestMapping(value = "/publico")
 public class PublicoResource {
+	@Autowired
+	private PublicoService service;
 	
 	@GetMapping
-	public ResponseEntity<Publico> findAll(){
-		Publico p = new Publico(1L, "Neto", "neto@gmail.com", "999999");
-		return ResponseEntity.ok().body(p);
+	public ResponseEntity<List<Publico>> findAll(){
+		List<Publico> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Publico> findById(@PathVariable Long id){
+		Publico obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/jneto/cgae/services/PublicoService.java
@@ -0,0 +1,27 @@
+package com.jneto.cgae.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jneto.cgae.entities.Publico;
+import com.jneto.cgae.repositories.PublicoRepository;
+
+@Service
+public class PublicoService {
+	@Autowired
+	private PublicoRepository repository;
+	
+	public List<Publico> findAll(){
+		return repository.findAll();
+	}
+	
+	public Publico findById(Long id) {
+		Optional<Publico> obj = repository.findById(id);
+		
+		return obj.get();
+	}
+	
+}
