fd21eeb6d63b8e72a59714a33715ec34e6002689
Neto Nascimento <netolima1992@gmail.com>
2022-02-01 12:26:01 -0300

    projeto criado

---
+++ src/main/java/com/jneto/cgae/CgaeApplication.java
@@ -0,0 +1,13 @@
+package com.jneto.cgae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jneto/cgae/CgaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jneto.cgae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
