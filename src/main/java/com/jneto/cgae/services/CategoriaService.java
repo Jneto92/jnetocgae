d87fb14a66699f47f2dd3d48a9b8821c8a5fc6fb
Neto Nascimento <netolima1992@gmail.com>
2022-02-07 15:34:36 -0300

    Tratamento de erros no deleteId

--- src/main/java/com/jneto/cgae/services/CategoriaService.java
+++ src/main/java/com/jneto/cgae/services/CategoriaService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.jneto.cgae.entities.Categoria;
 import com.jneto.cgae.repositories.CategoriaRepository;
+import com.jneto.cgae.services.exceptions.DatabaseException;
 import com.jneto.cgae.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class CategoriaService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+			}catch (EmptyResultDataAccessException e) {
+				throw new ResourceNotFoundException(id);
+			} catch (DataIntegrityViolationException e) {
+				throw new DatabaseException(e.getMessage());
+			}
 	}
 	
 	public Categoria update(Long id, Categoria obj) {
--- src/main/java/com/jneto/cgae/services/FuncionarioService.java
+++ src/main/java/com/jneto/cgae/services/FuncionarioService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.jneto.cgae.entities.Funcionario;
 import com.jneto.cgae.repositories.FuncionarioRepository;
+import com.jneto.cgae.services.exceptions.DatabaseException;
 import com.jneto.cgae.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class FuncionarioService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+			}catch (EmptyResultDataAccessException e) {
+				throw new ResourceNotFoundException(id);
+			} catch (DataIntegrityViolationException e) {
+				throw new DatabaseException(e.getMessage());
+			}
 	}
 	
 	public Funcionario update(Long id, Funcionario obj) {
